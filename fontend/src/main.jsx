0b8f0c820ac6252d3625dfaf260e0b4561f106a6
moan <moanfs1@gmail.com>
2024-08-26 16:59:20 +0700

    Add build files

--- fontend/src/main.jsx
+++ fontend/src/main.jsx
@@ -2,22 +2,22 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Web from './routes/web.jsx'
-// import axios from 'axios';
+import axios from 'axios';
 
 
-// axios.interceptors.request.use(
-//   config => {
-//     const authToken = localStorage.getItem('authToken');
-//     if (authToken) {
-//       config.headers.Authorization = `Bearer ${authToken}`;
-//       console.log('Token:', authToken);
-//     }
-//     return config;
-//   },
-//   error => {
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.request.use(
+  config => {
+    const authToken = localStorage.getItem('authToken');
+    if (authToken) {
+      config.headers.Authorization = `Bearer ${authToken}`;
+      console.log('Token:', authToken);
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 const midtransClientKey = import.meta.env.VITE_MIDTRANS_CLIENT_KEY;
 
